ab3c28c4df59eb8e56434d7e0ab972e139b9d5ff
Del0m <98620378+Del0m@users.noreply.github.com>
2023-09-17 23:23:35 -0400

    fixed cameratracking, created gravity

--- Formorian Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Formorian Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,26 +9,55 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Drag / Drop")]
-    public CharacterController player; // facilitate movement for character
-    
+    private EntityStatScript entity; // stat script where speed will be derived.
+
+    private CharacterController player; // facilitate movement for character
+    private Gravity gravityScript;
+
     // input system for player
     private Controls control;
     private Vector2 movement;
     private void Start() // initalizing controls
     {
-        control = new Controls();
-        control.Enable();
+        // grabbing objects off player
+        entity = gameObject.GetComponent<EntityStatScript>();
+        player = gameObject.GetComponent<CharacterController>();
+        gravityScript = gameObject.GetComponentInChildren<Gravity>();
+
+        control = new Controls(); // set controls so conflicting controls won't mess with movement
+        control.Enable(); // turn on action inputs
     }
     public void FixedUpdate() // updates player position
     {
-        player.Move(movement);
+        Movement(); // conduct movement
     }
     public void Move(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-            movement = control.Movement.Walk.ReadValue<Vector2>();
+            movement.x = control.Movement.Walk.ReadValue<Vector2>().x;
+            movement *= Time.deltaTime * entity.stats[StatBlock.Stats.speed]; // multiplying by DeltaTime and entity speed
+
         }
+    }
+    float falling;
+    void Gravity()
+    {
+        if (!gravityScript.grounded)
+        {
+            falling += Physics2D.gravity.y * Time.deltaTime;
+        }
+        else
+        {
+            falling = 0f;
+        }
+    }
+    void Movement()
+    {
+        Gravity();
+        movement.y = falling;
+        movement.y = Mathf.Clamp(movement.y, -10f, 10f) * Time.deltaTime;
+        player.Move(movement); // moves player "movement" increments every update.
+
     }
 }
