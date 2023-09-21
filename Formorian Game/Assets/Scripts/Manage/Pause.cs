a1908bd827d6ba3c13864f622db705f8b7da806e
Armin Delmo <armindelmo00@gmail.com>
2023-09-21 17:35:40 -0400

    uicontroller uses static to enable / disable ui; pause now has ui

--- Formorian Game/Assets/Scripts/Manage/Pause.cs
+++ Formorian Game/Assets/Scripts/Manage/Pause.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    public Image ui;
     private static bool gamePause;
     public static bool IsPaused
     {
         get { return gamePause; }
-        set { gamePause = !gamePause; } 
+        set 
+        { 
+            gamePause = !gamePause;
+        } 
     }
+    public void Menu(InputAction.CallbackContext ctx) // enable menu
+    {
+        try
+        {
+            if (ctx.performed)
+            {
+                if (gamePause) // check field, getset meant to be changed
+                {
+                    UIController.HideUI(ui);
+                    return;
+                }
+                UIController.ShowUI(ui);
+            }
+        }
+        catch (System.Exception)
+        {
+            Debug.LogError("Must set UI variable");
+            throw;
+        }
 
+    }
 }
 
