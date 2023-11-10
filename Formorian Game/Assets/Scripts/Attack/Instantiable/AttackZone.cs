65f08928e9648e21868d2472f054161b825a695a
Del0m <98620378+Del0m@users.noreply.github.com>
2023-11-10 10:52:19 -0500

    renamed asset

--- Formorian Game/Assets/Scripts/Attack/Instantiable/AttackZone.cs
+++ Formorian Game/Assets/Scripts/Attack/Instantiable/AttackZone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InstantiateAttack : MonoBehaviour
+public class AttackZone : MonoBehaviour
 {
     public string objTag;
 
