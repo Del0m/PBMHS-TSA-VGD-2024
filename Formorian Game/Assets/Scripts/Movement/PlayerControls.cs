58e45a4f3fc8cb35558e9e282c1c45b695a92549
Del0m <98620378+Del0m@users.noreply.github.com>
2024-01-25 22:01:11 -0500

    fixing player gravity

--- Formorian Game/Assets/Scripts/Movement/PlayerControls.cs
+++ Formorian Game/Assets/Scripts/Movement/PlayerControls.cs
@@ -49,7 +49,7 @@ public class PlayerControls : Movement
     }
     void Movement()
     {
-        rb.velocity = new Vector2(movement.x, Mathf.Clamp(rb.velocity.y, -20f * rb.gravityScale, 20f * rb.gravityScale)); // move player
+        rb.velocity = new Vector2(movement.x, rb.velocity.y + rb.gravityScale * Time.fixedDeltaTime); // move player
         
         // check player's direction
         if(rb.velocity.x < 0) // face left
